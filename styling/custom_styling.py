5fd4a297f3563b3adac8052d7715dd41d67163cc
Gazzamac10 <mccarthygary@hotmail.com>
2023-12-08 14:51:52 +0000

    v1

---
+++ styling/custom_styling.py
@@ -0,0 +1,93 @@
+# Add custom CSS styles
+custom_css = """
+<style>
+    h2 {
+        color: #333753;
+        font-family: Arial, sans-serif;
+        font-size: 78px;
+        text-align: left;
+    }
+    h3 {
+        font-weight: bold;
+        color: #104E8B;
+        font-family: Arial, sans-serif;
+        font-size: 30px;
+        text-align: left;
+    }
+    h4 {
+        font-weight: bold;
+        color: Black;
+        font-family: Arial, sans-serif;
+        font-size: 20px;
+        text-align: left;
+    }
+    h5 {
+        font-weight: bold;
+        color: Black;
+        font-family: Arial, sans-serif;
+        font-size: 18px;
+        text-align: left;
+    }
+    h6 {
+        font-weight: bold;
+        color: #333753;
+        font-family: Della;
+        font-size: 50px;
+        text-align: center;
+    }
+    p {
+        color: Black;
+        font-family: Arial, sans-serif;
+        font-size: 24px;
+        text-align: left;
+    }
+    pbold {
+        color: Black;
+        font-weight: bold;
+        font-family: Arial, sans-serif;
+        font-size: 18px;
+        text-align: left;
+    }
+    p2 {
+        color: Black;
+        font-family: Arial, sans-serif;
+        font-size: 18px;
+        text-align: left;
+        padding-left: 25px 
+    }
+    p3 {
+        color: Red;
+        font-family: Arial, sans-serif;
+        font-size: 18px;
+        text-align: left;
+        #margin-bottom: 24px;
+    }
+    p4 {
+        color: #d43a2e;
+        font-family: Arial, sans-serif;
+        font-size: 18px;
+        text-align: left;
+        #margin-bottom: 24px;
+    }
+    p5 {
+        color: #86325b;
+        font-family: Arial, sans-serif;
+        font-size: 24px;
+        text-align: left;
+        #margin-bottom: 24px;
+    }
+    dummy {
+        color: White;
+        font-family: Arial, sans-serif;
+        font-size: 18px;
+        text-align: left;
+        #line-height: 0.15;
+        #margin-bottom: 0px;
+    }
+    .katex-html {
+        text-align: left;
+        font-size: 22px;
+        font-family: 'Times New Roman', Times, serif;
+    }
+</style>
+"""
